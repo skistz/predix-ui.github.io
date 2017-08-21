14479b5c0c84281b00eb62ab6213f3aeef79a8e2
David Leonard <davidrobertleonard@gmail.com>
2017-08-21 11:56:02 -0700

    Kill fancy scroll watcher for jump nav, migrate scrolling behavior into px-catalog-page-viewer

--- scripts/page-builder/templates.js
+++ scripts/page-builder/templates.js
@@ -1,7 +1,7 @@
 const defaultTemplate = ({ content, anchors, metadata }) => `
   <link defer rel="import" href="${metadata.pathToRoot}bower_components/polymer/polymer.html" />
   <link defer rel="import" href="${metadata.pathToRoot}bower_components/px-demo/px-demo-footer.html" />
-  <link defer rel="import" href="${metadata.pathToRoot}bower_components/iron-media-query/iron-media-query.html" />
+  <link defer rel="import" href="${metadata.pathToRoot}bower_components/px-demo/px-demo-footer.html" />
   <link defer rel="import" href="${metadata.pathToRoot}elements/px-catalog-page/px-catalog-page-behavior.html" />
   <link defer rel="import" href="${metadata.pathToRoot}elements/px-catalog-page/px-catalog-page-elements.html" />
   <link defer rel="import" href="${metadata.pathToRoot}css/px-catalog-page-styles.html" />
@@ -13,7 +13,6 @@ const defaultTemplate = ({ content, anchors, metadata }) => `
       <style include="px-catalog-page-styles"></style>
       <style include="px-catalog-code-styles"></style>
       <style include="px-catalog-theme-styles"></style>
-      <iron-media-query query="[[_tocCollapseUnder]]" query-matches="{{_shouldCollapseToc}}"></iron-media-query>
 
       <div class="page">
         ${anchors.length ? anchorListPartial(anchors) : ''}
@@ -23,6 +22,7 @@ const defaultTemplate = ({ content, anchors, metadata }) => `
           ${content}
         </div>
       </div>
+      <px-demo-footer></px-demo-footer>
     </template>
     <script>
       Polymer({
@@ -43,7 +43,7 @@ const anchorListPartial = anchors => `
 
 const anchorItemPartial = ({title, slug, children}) => `
   <li class="toc__item">
-    <a class="toc__item__link" href="#" anchor="#${slug}" on-tap="_handleTocTapped">${title}</a>
+    <a class$="toc__item__link [[_getTocLinkClassName('#${slug}', activeAnchor)]]" href="#" anchor="#${slug}" on-tap="_handleAnchorTapped">${title}</a>
     ${children && children.length ? `<ul class="toc">${children.map(anchorItemPartial).join('')}</ul>` : ''}
   </li>
 `;
